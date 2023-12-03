252b06554f926e5d48ac89842c7b1bbdb398363b
Vladislav Petrov <grafv1ad@yandex.com>
2023-12-03 21:11:21 +0500

    made up page

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import { useState } from "react";
 
 import viteLogo from "../public/vite.svg";
-import "./App.css";
+import hhLogo from "./assets/hh.svg";
 import reactLogo from "./assets/react.svg";
+import vladFace from "./assets/vlad.png";
+
+import "./App.less";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -16,19 +19,21 @@ function App() {
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
+        <a href="https://hh.ru" target="_blank">
+          <img src={hhLogo} className="logo hh" alt="HH logo" />
+        </a>
+        <a href="https://vladpetrov.me" target="_blank">
+          <img src={vladFace} className="logo vlad" alt="Vlados :)" />
+        </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Vite + React + HH + Vlad</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+        <div className="wow">＼(^ o ^)／</div>
+        <p>Во славу бандлеров!</p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   );
 }
